ef8c854f868490b4df309266152ba460dea09b52
PileJL <pilejlc@gmail.com>
2025-02-03 15:12:50 +0800

    product creation feature done

--- intern1_test/app/Http/Controllers/Api/ProductController.php
+++ intern1_test/app/Http/Controllers/Api/ProductController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class ProductController extends Controller
 {
@@ -13,7 +14,7 @@ class ProductController extends Controller
         $products = Product::get();
 
         // check if there are records
-        if ($products) {
+        if ($products->countBy() > 0) {
             return ProductResource::collection($products);
         }
         else {
@@ -21,8 +22,31 @@ class ProductController extends Controller
         }
     }
 
-    public function store() {
+    public function store(Request $request) {
+        // validation
+        $validator = Validator::make($request->all(), [
+            'name' => 'required|string|max:255',
+            'description' => 'required',
+            'price' => 'required|integer'
+        ]);
 
+        if($validator->fails()) {
+            return response()->json([
+                'message' => "All fields are mandatory",
+                'error' => $validator->messages()
+            ], 422);
+        }
+        // add to database
+        $product = Product::create([
+            'name' => $request->name,
+            'description' => $request->description,
+            'price' => $request->price
+        ]);
+
+        return response()->json([
+            'message' => 'Product Created Successfully',
+            'data' => new ProductResource($product)
+        ], 200);
     }
 
     public function show() {
--- intern1_test/routes/web.php
+++ intern1_test/routes/web.php
@@ -6,5 +6,3 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-
-// Route::get('/all_products', [ProductController::class, 'show']);
